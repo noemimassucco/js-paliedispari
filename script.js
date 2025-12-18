49935e0291fd35b4c9bae0457d5ef78989ce388a
NoemiMassucco <noemimassucco@gmail.com>
2025-12-18 20:51:32 +0100

    eseguito

---
+++ script.js
@@ -0,0 +1,25 @@
+// 1. Chiediamo all’utente di inserire una parola
+const parolaUtente = prompt("Inserisci una parola");
+
+// 2. Creiamo una funzione per verificare se è palindroma
+function isPalindroma(parola) {
+  // invertiamo la parola
+  const parolaInversa = parola.split("").reverse().join("");
+
+  // confrontiamo parola originale e invertita
+  if (parola === parolaInversa) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// 3. Usiamo la funzione
+const risultato = isPalindroma(parolaUtente);
+
+// 4. Stampiamo il risultato
+if (risultato) {
+  console.log("La parola è palindroma");
+} else {
+  console.log("La parola non è palindroma");
+}
